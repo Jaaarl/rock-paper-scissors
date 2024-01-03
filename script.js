3c19548699d91d06863838092660c209d6156afb
Jarl <152849999+Jaaarl@users.noreply.github.com>
2024-01-04 00:39:54 +0800

    added a bo5 function with ui

--- script.js
+++ script.js
@@ -13,52 +13,63 @@ function getComputerChoice() {
 }
 function playerSelection() {
     let choice = null;
-    //     choice = prompt("Choose your weapon! Rock Paper Scissors?");
-    //     while (true) {
-    //         if (choice == "rock" || choice == "paper" || choice == "scissors") {
-    //             return choice;
-    //             break;
-    //         }
-    //         else {
-    //             choice = prompt("Wrong answer Try again! Choose your weapon! Rock Paper Scissors?");
-    //             choice = choice.toLowerCase();
-    //         }
-    //     }
 }
 function game(playerChoice) {
-    // let humanScore = 0;
-    // let compScore = 0;
 
     let compChoice = getComputerChoice();
 
     let decision;
     if (playerChoice == "scissors" && compChoice == "rock") {
         decision = playerChoice + " vs " + compChoice + " You Lost! ";
+        compScore++;
     }
     else if (playerChoice == "scissors" && compChoice == "paper") {
         decision = playerChoice + " vs " + compChoice + " You Win!";
+        humanScore++;
     }
     else if (playerChoice == "rock" && compChoice == "paper") {
         decision = playerChoice + " vs " + compChoice + " You lost!";
+        compScore++;
     }
     else if (playerChoice == "rock" && compChoice == "scissors") {
         decision = playerChoice + " vs " + compChoice + " You win!";
+        humanScore++;
     }
     else if (playerChoice == "paper" && compChoice == "rock") {
         decision = playerChoice + " vs " + compChoice + " You win!";
+        humanScore++;
+
     }
     else if (playerChoice == "paper" && compChoice == "scissors") {
         decision = playerChoice + " vs " + compChoice + " You lost!";
+        compScore++;
     }
     else {
         decision = playerChoice + " vs " + compChoice + " Its a Tie! ";
     }
     let div = document.querySelector('div');
     div.textContent = decision;
+    //const score = document.querySelector('#score');
+    // score.textContent = "Human - " + humanScore + " AI - " + compScore;
+    const score = document.createElement('p')
+    score.textContent = "Human - " + humanScore + " AI - " + compScore;
+
+    div.appendChild(score);
+    if (compScore == 5 || humanScore == 5) {
+        let msg = document.createElement('h1');
+        if (compScore == 5) msg.textContent = "AI won! Try again";
+        else msg.textContent = "Human won! Try again";
+        compScore = 0;
+        humanScore = 0;
+        div.appendChild(msg);
+    }
+
 }
 
 
 const btnRock = document.querySelector('#rock');
+let humanScore = 0;
+let compScore = 0;
 
 btnRock.addEventListener('click', () => {
     game('rock');
